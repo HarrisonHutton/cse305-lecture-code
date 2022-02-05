28263925cd01f682649cfccf6147de6c79197070
HarrisonHutton <harrison.hutton6@gmail.com>
2022-02-05 13:07:53 -0500

    Explain concatenation abstraction

--- lecture2/abstraction.ml
+++ lecture2/abstraction.ml
@@ -1,18 +1,20 @@
 (*
-  Will "return" 25
+  Will return 25
 *)
 let x = 2 + 3 in
   x * x;;
 
 (*
-  Will "return" 32
+  Will return 32
 *)
 let y = 2 in
   let squared = y * y in 
     let cubed = y * squared in 
       cubed * squared;;
 
-
+(*
+  Will return "aaabbb"
+*)
 let a = "a" in 
   let b = "b" in 
     let c = a ^ a ^ a in 
